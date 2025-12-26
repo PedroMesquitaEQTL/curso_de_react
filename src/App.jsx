7eb7a8b0298104425d3400ed5eed2a0ee9770d5a
PedroMesquitaEQTL <pedro.mesquita@equatorialservicos.com.br>
2025-12-26 17:48:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      descritption: "Ler a documentação do React e fazer exercícios",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Fazer compras",
+      descritption: "Comprar itens necessários para a cozinha",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Outro title",
+      descritption: "Comprar itens ",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // Preciso atualiza essa tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // Não preciso atualizar essa tarefa
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
